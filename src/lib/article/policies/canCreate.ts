95178fec4e93512884bc46c2c98e361de9892ec4
Maximiliano Ivan Biondo <mbiondo@hcdn.gob.ar>
2024-09-02 15:51:18 -0300

    feat: Add user confirmation check in article creation policy

--- src/lib/article/policies/canCreate.ts
+++ src/lib/article/policies/canCreate.ts
@@ -3,6 +3,7 @@ import type { ArticlePolicy } from '../types'
 const CanCreatePolicy: ArticlePolicy = {
   test: (ctx) => {
     if (!ctx.user) return false
+    if (!ctx.user.confirmed) return false
     return ctx.user.role === 'user' || ctx.user.role === 'admin'
   },
 }
