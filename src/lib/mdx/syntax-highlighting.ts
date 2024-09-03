bc0458a8746b99310aa1223ad14a6c802ff0fac4
Maximiliano Biondo <mbiondo@hcdn.gob.ar>
2024-09-03 00:48:47 -0300

    feat: Fix charset typo in layout.astro and improve code highlighting in syntax-highlighting.ts

--- src/lib/mdx/syntax-highlighting.ts
+++ src/lib/mdx/syntax-highlighting.ts
@@ -28,14 +28,14 @@ const highlighter = await createHighlighter({
 })
 
 export const syntaxHighlighting = () => {
-  const codeBlocks = document.querySelectorAll('pre code')
-  codeBlocks.forEach((codeBlock) => {
+  const codeBlocks = document.querySelectorAll('pre code') as NodeListOf<HTMLDivElement>
+  codeBlocks.forEach((codeBlock: HTMLDivElement) => {
     const lang = codeBlock.classList[0].replace('language-', '').trim()
 
     //validate if the language is supported
     if (!highlighter.getLanguage(lang)) return
 
-    const codeHTML = highlighter.codeToHtml(codeBlock.innerHTML, {
+    const codeHTML = highlighter.codeToHtml(codeBlock.innerText, {
       lang: lang,
       theme: 'dracula',
     })
@@ -45,10 +45,8 @@ export const syntaxHighlighting = () => {
       const code = codeHTML
         .replace('<pre class="shiki dracula" style="background-color:#282A36;color:#F8F8F2" tabindex="0">', '')
         .replace('</pre>', '')
-      pre.classList.add('shiki')
-      pre.classList.add('dracula')
-
-      pre.innerHTML = code
+      console.log(code)
+      pre.setHTMLUnsafe(code)
     }
   })
 }
