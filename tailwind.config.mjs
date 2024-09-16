5b5806055b1252f82c1e2b3522b5ce1a4a0f1b1a
Maximiliano Biondo <mbiondo@hcdn.gob.ar>
2024-09-16 11:20:10 -0300

    Refactor avatar component to use UTC string for updatedAt date

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,6 @@ export default {
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
-    themes: ['light', 'dracula'],
+    themes: ['light', 'dracula', 'black'],
   },
 }
